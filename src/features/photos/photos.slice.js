8e4f58970ffe095ee9bd257761f0c81dd8eba0fc
Benjamin Hill <benjaminh@indeed.com>
2024-01-03 11:42:38 -0800

    Completed redux assessment

--- src/features/photos/photos.slice.js
+++ src/features/photos/photos.slice.js
@@ -1,22 +1,35 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { selectSearchTerm } from '../search/search.slice';
-import photos from './photos.data.js';
+import { createSlice } from "@reduxjs/toolkit";
+import { selectSearchTerm } from "../search/search.slice";
+import photos from "./photos.data.js";
 
 const initialState = {
   photos,
 };
 
 const options = {
-  name: 'photos',
+  name: "photos",
   initialState,
   reducers: {
-    // Task 1: Create an `addPhoto()` case reducer that adds a photo to state.photos. 
-    // Task 1 Hint: You can use state.photos.unshift()
-    // `unshift()` documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift
-   
+    // Task 1: Create an `addPhoto()` case reducer that adds a photo to state.photos.
+    addPhoto: (state, action) => {
+      // Task 1 Hint: You can use state.photos.unshift()
+      // `unshift()` documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift
+      state.photos.unshift({
+        id: state.photos.length + 1,
+        caption: action.payload.caption,
+        imageUrl: action.payload.imageUrl,
+      });
+    },
+
     // Task 6: Create an `removePhoto()` case reducer that removes a photo from state.photos
-    // Task 6 Hint: You can use state.photos.splice()
-    // `splice()` documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+    removePhoto: (state, action) => {
+      // Task 6 Hint: You can use state.photos.splice()
+      // `splice()` documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+      state.photos.splice(
+        state.photos.findIndex((photo) => photo.id === action.payload),
+        1
+      );
+    },
   },
 };
 
@@ -29,4 +42,13 @@ export default photosSlice.reducer;
 export const selectAllPhotos = (state) => state.photos.photos;
 export const selectFilteredPhotos = (state) => {
   // Task 12: Complete `selectFilteredPhotos()` selector to return a filtered list of photos whose captions match the user's search term
+  const searchTerm = selectSearchTerm(state);
+
+  if (!searchTerm) {
+    return state.photos.photos;
+  }
+
+  return state.photos.photos.filter((photo) =>
+    photo.caption.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 };
